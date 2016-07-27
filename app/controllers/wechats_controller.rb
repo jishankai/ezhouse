8d96ba99347d22f99ecfb2269bf4d2519c0c3d3e
skji <jishankai@qq.com>
2016-07-27 11:30:08 +0800

    update wechat

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -6,16 +6,33 @@ class WechatsController < ActionController::Base
   #   request.reply.text "#{content}" # Just echo
   # end
 
-  on :text, with: /^[男|女]\+\d+\+\d+\+\d+$/ do |request|
-    request.reply.text "非常感谢参与抽奖，你猜的数一定很大吧？！
+  on :text, with: /^[男|女]\ \d+\ \d{11}\ \d+$/ do |request|
+    request.reply.text "非常感谢参与抽奖，你猜的数一定很大吧？！/::P
 
 把赠话剧票活动分享到朋友圈，让大家帮你早日达到目标，并与白富美或高富帅配对！"
   end
 
+  on :text, with: '北漂' do |request|
+    request.reply.image('wfjD9PAgZwMe__i0uqLCnvg-9yhkCGMhhjJMnD8BoZE')
+  end
+  on :text, with: '大学生' do |request|
+    request.reply.image('wfjD9PAgZwMe__i0uqLCnjinACcdGbbZpHs4RzrsEEo')
+  end
+  on :text, with: '地区' do |request|
+    request.reply.text "请访问http://ehero-info.sxl.cn获取地区群二维码列表"
+  end
+
   on :event, with: 'subscribe' do |request|
-    request.reply.text "谢谢您关注我们的公众号！
-易房好介，为您推荐最匹配的房屋和经纪人！
-请访问我们正在开发的网站http://ehero.cc，提供您宝贵的意见！"
+    request.reply.text "您好，欢迎您关注易房好介~
+“易房好介”是一个大数据房产经纪人匹配平台，旨在为租房者与买房者提供更好的房产交易环境。
+平台在测试阶段，您目前可以免费试用“免扰电话”功能，如果您很喜欢，欢迎推荐给朋友~如果觉得哪里做的不够好，更欢迎您在公众号后台与我们沟通改进！
+
+【租房微信群】
+回复“北漂”，进入【北漂租房大群】
+
+回复“大学生”，进入【北京大学生租房合租信息群】
+
+回复“地区”，进入【按地区划分租房微信群】"
   end
 
   on :click, with: 'ABOUTUS' do |request, key|
