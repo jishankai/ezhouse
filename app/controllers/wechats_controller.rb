4296d686359f4d16af5ac256a8ea643b75fdf984
skji <jishankai@qq.com>
2016-08-14 14:38:37 +0800

    update layout

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -36,16 +36,26 @@ class WechatsController < ActionController::Base
   end
 
   on :click, with: 'ABOUTUS' do |request, key|
-    request.reply.text "易房好介（http://ehero.cc）是一家互联网房地产创业公司，致力于帮助客户找到最匹配的房屋与经纪人。
-公司已拿到百万级的种子投资。团队成员来自芝加哥大学，清华，北大，浙大等名校，并均有海外工作背景。
-种子投资人与顾问团队包括清华统计学教授，高盛高管与量化基金经理等。
+    request.reply.text "我们是一家互联网房地产创业公司，致力于帮助客户找到最匹配的房屋与经纪人。"
+  end
 
-公司地址：五道口清华科技园内
-商业合作请发邮件至杨阳洋（lyang419@gmail.com）
-技术合作请发邮件至季多宝（jishankai@qq.com）"
+  on :click, with: 'COMMENT' do |request, key|
+    request.reply.text "请前去我们的网站ehero.cc搜索经纪人并做出评价哦～"
+  end
+
+  on :click, with: 'GROUPS' do |request, key|
+    request.reply.text "回复“群主”，进入【大学生］，［北漂］或［地区］租房群"
   end
 
   on :click, with: 'HELP' do |request, key|
-    request.reply.text "公众号功能正着急开发中，您可以访问我们的网站http://ehero.cc"
+    request.reply.text "您好，欢迎您关注易房好介!
+
+“易房好介”是一个大数据房产经纪人匹配平台，旨在为租房者与买房者提供更好的房产交易环境。您成为了易房好介第一批的种子用户！
+
+平台在测试阶段，您目前可以免费试用“免扰电话”功能，如果您很喜欢，欢迎推荐给朋友~如果觉得哪里做的不够好，更欢迎您在公众号后台与我们沟通改进！
+
+【租房微信群】
+
+回复“群主”，进入【大学生］，［北漂］或［地区］租房群"
   end
 end
